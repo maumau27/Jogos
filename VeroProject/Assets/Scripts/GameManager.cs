bf349bad823486acb98eec29ab862d7cbff5be59
konado <mauriciolana@hotmail.com>
2017-05-25 15:08:32 -0300

    Commit com: -Comit inicial do VeroProject

---
+++ VeroProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+	// Use this for initialization
+	void Start () {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
